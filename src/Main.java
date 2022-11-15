375f2eaa652f980db1d5cd1def7076c19c189962
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2022-11-15 22:16:04 +0300

    refactor: оптимизировать метод updateSubTask

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
         Manager manager = new Manager();
 
-        // Создаем 1 Эпик с 2 подзадачами
+        //Создаем 1 Эпик с 2 подзадачами
         manager.setEpicTask(new EpicTask("Построить дом", "1й этап"));
         manager.setSubTask(new SubTask("Сделать фундамент", "Залить плиту 300 мм", 1), manager.getEpicTaskById(1));
         manager.setSubTask(new SubTask("Построить стены", "Газобетон 375 мм", 1), manager.getEpicTaskById(1));
@@ -28,8 +28,8 @@ public class Main {
         System.out.println(manager.getListSubTask());
 
         //Изменяем статусы созданных объектов и обновляем
-        manager.updateSubTask((manager.getSubTaskById(2)),  Status.IN_PROGRESS);
-        manager.updateSubTask(manager.getSubTaskById(5), Status.DONE);
+        manager.updateSubTask(new SubTask(2,"Сделать фундамент", "Залить плиту 300 мм", Status.IN_PROGRESS , 1));
+        manager.updateSubTask(new SubTask(5, "Заказать окна", "Стеклопакеты", Status.DONE, 4));
 
         // Печатаем списки после изменения статусов
         System.out.println();
@@ -38,8 +38,10 @@ public class Main {
         System.out.println();
 
         // Удаляем задачу из одного из эпиков
-        manager.removeByIdEpicTas(4);
+        manager.removeByIdEpicTask(4);
         System.out.println(manager.getListEpicTask());
         System.out.println(manager.getListSubTask());
+
+        manager.getTaskById(11);
     }
 }
