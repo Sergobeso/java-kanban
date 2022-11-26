68071e4fab2eb17e88b5b8762e18cf70a429eb23
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2022-11-26 13:17:41 +0300

    refactor: добавлены комментарии

--- src/services/TaskManager.java
+++ src/services/TaskManager.java
@@ -5,35 +5,48 @@ import modules.SubTask;
 import modules.Task;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public interface TaskManager<Tasks extends Task > {
+/** Интерфейс описывающий взаимодействие с задачами
+* Имеет следующую функциональность:
+         * - создает задачи
+         * - получает задачи
+         * - обновляет задачи
+         * - удаляет задачи
+*/
 
+public interface TaskManager {
+
+    // Создание задачи
     void setTask(Task task);
     void setEpicTask(EpicTask epicTask);
     void setSubTask(SubTask subTask, EpicTask epicTask);
 
+    // Получение списка всех задач.
     ArrayList<Task> getListTask();
     ArrayList<EpicTask> getListEpicTask();
     ArrayList<SubTask> getListSubTask();
 
+    // Удаление всех задач.
     void clearTask();
     void clearEpicTaskMap();
     void clearSubTaskMap();
 
+    //Получение по идентификатору.
     Task getTaskById(int id);
     EpicTask getEpicTaskById(int id);
     SubTask getSubTaskById(int id);
 
+    //Обновление. Новая версия объекта с верным идентификатором передаётся в виде параметра.
     void updateTask(Task task);
     void updateEpicTaskMap(EpicTask epicTask);
     void updateSubTask(SubTask subTask);
 
+    // Удаление по идентификатору.
     void removeByIdTask(int id);
     void removeByIdEpicTask(int id);
     void removeByIdSubTask(int id);
 
+    // Получение списка всех подзадач определённого эпика.
     ArrayList<SubTask> getListSubTask(EpicTask epicTask);
 
-   List<Tasks> getHistory();
 }
