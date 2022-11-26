8d92bd0f290e7f5ab09135c100ffe0e8a4dbe4a0
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2022-11-26 11:47:34 +0300

    refactor: переименоватьать класс в InMemoryTaskManager refactor: добавить implements TaskManager feat: добавить функцию добавление задач в историю просмотра

--- src/services/InMemoryTaskManager.java
+++ src/services/InMemoryTaskManager.java
@@ -6,6 +6,7 @@ import modules.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -18,26 +19,30 @@ import java.util.Optional;
  * - обновляет статус EpicTask
  */
 
-public class Manager {
+public class InMemoryTaskManager<Tasks extends Task> implements TaskManager  {
     private int id;
-
-    private HashMap<Integer, Task> taskMap = new HashMap<>();
-    private HashMap<Integer, EpicTask> epicTaskMap = new HashMap<>();
-    private HashMap<Integer, SubTask> subTaskMap = new HashMap<>();
+    private static final int MAX_LENGTH_IN_HISTORY = 10;
+    private final HashMap<Integer, Task> taskMap = new HashMap<>();
+    private final HashMap<Integer, EpicTask> epicTaskMap = new HashMap<>();
+    private final HashMap<Integer, SubTask> subTaskMap = new HashMap<>();
+    public List<Tasks> historyIdList = new ArrayList<>();
 
     // Создание задачи
+    @Override
     public void setTask(Task task) {
         id++;
         task.setId(id);
         taskMap.put(task.getId(), task);
     }
 
+    @Override
     public void setEpicTask(EpicTask epicTask) {
         id++;
         epicTask.setId(id);
         epicTaskMap.put(epicTask.getId(), epicTask);
     }
 
+    @Override
     public void setSubTask(SubTask subTask, EpicTask epicTask) {
         id++;
         subTask.setId(id);
@@ -46,59 +51,75 @@ public class Manager {
     }
 
     // Получение списка всех задач.
+    @Override
     public ArrayList<Task> getListTask() {
         return new ArrayList<>(taskMap.values());
     }
 
+    @Override
     public ArrayList<EpicTask> getListEpicTask() {
         return new ArrayList<>(epicTaskMap.values());
     }
 
+    @Override
     public ArrayList<SubTask> getListSubTask() {
         return new ArrayList<>(subTaskMap.values());
     }
 
     // Удаление всех задач.
+    @Override
     public void clearTask() {
         taskMap.clear();
     }
 
+    @Override
     public void clearEpicTaskMap() {
         epicTaskMap.clear();
     }
 
+    @Override
     public void clearSubTaskMap() {
         subTaskMap.clear();
     }
 
     //Получение по идентификатору.
+    @Override
     public Task getTaskById(int id) {
+        addIdHistory((Tasks) taskMap.get(id));
         return Optional.ofNullable(taskMap.get(id)).orElseThrow(() -> new NullPointerException("ID не найден"));
     }
 
+    @Override
     public EpicTask getEpicTaskById(int id) {
+        addIdHistory((Tasks) epicTaskMap.get(id));
         return Optional.ofNullable(epicTaskMap.get(id)).orElseThrow(() -> new NullPointerException("ID не найден"));
     }
 
+    @Override
     public SubTask getSubTaskById(int id) {
+        addIdHistory((Tasks) subTaskMap.get(id));
         return Optional.ofNullable(subTaskMap.get(id)).orElseThrow(() -> new NullPointerException("ID не найден"));
     }
 
     //Обновление. Новая версия объекта с верным идентификатором передаётся в виде параметра.
+    @Override
     public void updateTask(Task task) {
         taskMap.put(task.getId(), task);
     }
 
+    @Override
     public void updateEpicTaskMap(EpicTask epicTask) {
         epicTaskMap.put(epicTask.getId(), epicTask);
     }
 
+    @Override
     public void updateSubTask(SubTask subTask) {
         subTaskMap.put(subTask.getId(), subTask);
         updateStatus(subTask.getEpicId());
     }
 
     // Удаление по идентификатору.
+    @Override
     public void removeByIdTask(int id) {
         if (!taskMap.containsKey(id)) {
             return;
@@ -106,16 +127,18 @@ public class Manager {
         taskMap.remove(id);
     }
 
+    @Override
     public void removeByIdEpicTask(int id) {
         if (!epicTaskMap.containsKey(id)) {
             return;
         }
-        for (Integer val: epicTaskMap.get(id).getListSubTaskId()) {
+        for (Integer val : epicTaskMap.get(id).getListSubTaskId()) {
             removeByIdSubTask(val);
         }
         epicTaskMap.remove(id);
     }
 
+    @Override
     public void removeByIdSubTask(int id) {
         if (!subTaskMap.containsKey(id)) {
             return;
@@ -124,12 +147,26 @@ public class Manager {
     }
 
     // Получение списка всех подзадач определённого эпика.
+    @Override
     public ArrayList<SubTask> getListSubTask(EpicTask epicTask) {
-          ArrayList<SubTask> listSubTask = new ArrayList<>();
-            for (Integer key : epicTask.getListSubTaskId()) {
-                listSubTask.add(subTaskMap.get(key));
-            }
+        ArrayList<SubTask> listSubTask = new ArrayList<>();
+        for (Integer key : epicTask.getListSubTaskId()) {
+            listSubTask.add(subTaskMap.get(key));
+        }
         return listSubTask;
+    }
+
+    @Override
+    public List<Tasks> getHistory() {
+        return historyIdList;
+    }
+
+    // метод проверяет список на величину списка и добавляет в историю просмотров
+    private void addIdHistory(Tasks t) {
+        if (historyIdList.size() == MAX_LENGTH_IN_HISTORY) {
+            historyIdList.remove(0);
+            historyIdList.add(t);
+        } else historyIdList.add(t);
     }
 
 
@@ -170,9 +207,10 @@ public class Manager {
             } else {
                 epicTaskMap.get(epicId).setStatus(Status.IN_PROGRESS);
             }
-        } catch(NullPointerException e){
-                System.out.println(("Список задач пуст"));
-           }
+        } catch (NullPointerException e) {
+            System.out.println(("Список задач пуст"));
+        }
     }
-}
 
+
+}
