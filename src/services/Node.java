c7a28793ce0b69c50479621efa23af1d072dad66
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2022-12-19 20:20:37 +0300

    refactor: заменить модификатор доступа на private

--- src/services/Node.java
+++ src/services/Node.java
@@ -1,13 +1,37 @@
 package services;
 
 public class Node<E> {
-    protected E task;
-    protected Node<E> next;
-    protected Node<E> prev;
+    private E element;
+    private Node<E> next;
+    private Node<E> prev;
 
-    protected Node(Node<E> prev, E task, Node<E> next){
-        this.task = task;
+    public Node(Node<E> prev, E element, Node<E> next){
+        this.element = element;
         this.next = next;
+        this.prev = prev;
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+    public void setElement(E task) {
+        this.element = task;
+    }
+
+    public Node<E> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<E> next) {
+        this.next = next;
+    }
+
+    public Node<E> getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node<E> prev) {
         this.prev = prev;
     }
 }
