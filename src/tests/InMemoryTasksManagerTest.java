28657dfedb9132145b885dbb172fb9f096eb0965
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-01-27 12:52:00 +0300

    feature: реализовать ТЕСТЫ менеджера InMemoryTasksManager

--- src/tests/InMemoryTasksManagerTest.java
+++ src/tests/InMemoryTasksManagerTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import managers.InMemoryTaskManager;
 import org.junit.jupiter.api.BeforeEach;
-
+/**
+ * Класс описывающий реализацию ТЕСТОВ менеджера InMemoryTasksManager.
+ */
 public class InMemoryTasksManagerTest extends TaskManagerTest<InMemoryTaskManager>{
     @BeforeEach
     public void setUp() {
